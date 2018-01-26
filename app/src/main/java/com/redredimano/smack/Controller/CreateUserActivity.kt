22c337e3c998ae1e6de208918c650df78d0b7f16
Generoso Redimano Jr <gredimano@objectbright.com>
2018-01-26 12:54:58 +0800

    Lesson 8.

--- app/src/main/java/com/redredimano/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/redredimano/smack/Controller/CreateUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.redredimano.smack.R
 import com.redredimano.smack.Services.AuthService
+import com.redredimano.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -51,6 +52,7 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun createUserClicked(view: View) {
 
+        val username = createUsernameText.text.toString()
         val email = createEmailAddressText.text.toString()
         val password = createPasswordText.text.toString()
 
@@ -58,8 +60,14 @@ class CreateUserActivity : AppCompatActivity() {
             if (registerSuccess) {
                 AuthService.loginUser(this, email, password) { loginSuccess ->
                     if (loginSuccess) {
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
+                        AuthService.createUser(this, username, email, userAvatar, avatarColor) { createSuccess ->
+                            if (createSuccess) {
+                                println(UserDataService.avatarName)
+                                println(UserDataService.avatarColor)
+                                println(UserDataService.name)
+                                finish()
+                            }
+                        }
                     }
                 }
             }
