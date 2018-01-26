bc312d280161a23e1266e6c2c4a785ee64d861e2
Generoso Redimano Jr <gredimano@objectbright.com>
2018-01-26 14:25:54 +0800

    Lesson 11.

--- app/src/main/java/com/redredimano/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/redredimano/smack/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.redredimano.smack.R
+import com.redredimano.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,6 +21,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
 
+
+        AuthService.loginUser(this, email, password) { loginSuccess ->
+            if (loginSuccess) {
+                AuthService.findUserByEmail(this) { findSuccess ->
+                    if (findSuccess) {
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/redredimano/smack/Services/AuthService.kt
+++ app/src/main/java/com/redredimano/smack/Services/AuthService.kt
@@ -1,15 +1,15 @@
 package com.redredimano.smack.Services
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.redredimano.smack.Utilities.URL_CREATE_USER
-import com.redredimano.smack.Utilities.URL_LOGIN
-import com.redredimano.smack.Utilities.URL_REGISTER
+import com.redredimano.smack.Utilities.*
 import org.json.JSONException
 import org.json.JSONObject
 
@@ -133,5 +133,40 @@ object AuthService {
         }
 
         Volley.newRequestQueue(context).add(createRequest)
+    }
+
+    fun findUserByEmail(context: Context, complete: (Boolean) -> Unit) {
+        val findUserRequest = object : JsonObjectRequest(Method.GET, "$URL_GET_USER$userEmail", null, Response.Listener { response ->
+            try {
+                UserDataService.id = response.getString("_id")
+                UserDataService.name = response.getString("name")
+                UserDataService.email = response.getString("email")
+                UserDataService.avatarName = response.getString("avatarName")
+                UserDataService.avatarColor = response.getString("avatarColor")
+
+                val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                LocalBroadcastManager.getInstance(context).sendBroadcast(userDataChange)
+                complete(true)
+
+            } catch (e: JSONException) {
+                Log.d("JSON", "EXC: " + e.localizedMessage)
+            }
+
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR", "Could not find user.")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer $authToken")
+                return headers
+            }
+        }
+
+        Volley.newRequestQueue(context).add(findUserRequest)
     }
 }
